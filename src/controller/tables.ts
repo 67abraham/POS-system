191b6760f22606056504005120ecb148f936938a
dev.Abraham67 <abrahamcsackie160@gmail.com>
2026-07-17 04:20:28 +0800

    table

--- src/controller/tables.ts
+++ src/controller/tables.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { logger } from "../lib/logger";
 import { prisma } from "../lib/prisma";
 import { activityLog } from "../lib/activityLog";
+import { TableStatus } from "../../generated/prisma/enums";
 
 
 export const createTable = async(req:Request, res:Response)=>{
@@ -9,6 +10,8 @@ export const createTable = async(req:Request, res:Response)=>{
         const {tableName, seats, image} = req.body;
         if(!tableName || !seats) return res.status(401).json({message: "All field required"})
         
+            const checkName = await prisma.table.findFirst({where: {tableName}})
+            if(checkName)return res.status(401).json({message: "Table Exist"})
         const creTable = await prisma.table.create({
             data:{
                 tableName,
@@ -38,7 +41,9 @@ export const updateTable = async(req:Request, res:Response)=>{
         const {id}= req.params as {id:string}
         const {tableName, seats, image} = req.body;
         if(!tableName || !seats) return res.status(401).json({message: "All field required"})
-        
+        const checkName = await prisma.table.findFirst({where: {tableName}})
+        if(checkName)return res.status(401).json({message: "Table Exist"})
+    
         const upTable = await prisma.table.update({
             where: {id},
             data:{
@@ -48,14 +53,14 @@ export const updateTable = async(req:Request, res:Response)=>{
             }
         });
 
-             await activityLog({
+            await activityLog({
                     userId: (req as any).user?.id,
                     details: `UpDate Table: ${upTable.tableName}`,
                     actions:`Update TABLE`
                         });
                 
-                logger.info(`Table Created: ${upTable.tableName}`)
-                res.status(201).json(upTable)
+            logger.info(`Table Created: ${upTable.tableName}`)
+            res.status(201).json(upTable)
                 
         
     } catch (error) {
@@ -80,10 +85,55 @@ export const deleteTable= async(req:Request, res:Response)=>{
                         });
                 
                 logger.info(`Table Created: ${delTable.tableName}`)
-                res.status(201).json({message: "Table Deleted Successful"})
+                res.status(200).json({message: "Table Deleted Successful"})
                 
     } catch (error) {
         logger.info("Internal Server Error")
+        res.status(500).json({message: "Internal Server Error"})
+          
+    }
+}
+
+export const getTable =async(req:Request, res:Response)=>{
+    try {
+
+        const page = Math.max(1, parseInt(req.query.page as any) || 1);
+        const limit = Math.max(1, parseInt(req.query.limit as any) || 10);
+
+        const skip = (page - 1) * limit;
+
+        const available = (req as any).user.role ==="ADMIN"? undefined : TableStatus.AVAILABLE;
+
+        const [getT, totalTable] = await Promise.all([
+            prisma.table.findMany({
+                where:{tableStatus: available},
+                take: limit,
+                skip,
+                orderBy:{
+                    tableName: "asc"
+                }
+            }),
+            prisma.table.count({where:{tableStatus: available}})
+
+
+        ]);
+
+        const totalPage = Math.ceil(totalTable / limit);
+
+                
+        logger.info(`Get ALl Table`)
+         res.status(200).json({
+            getT,
+            totalPage,
+            totalTable,
+            limit,
+            skip,
+            hasNextPage: page < totalPage,
+            hasPrevPage: page > 1
+         })
+                
+    } catch (error) {
+       logger.info("Internal Server Error")
         res.status(500).json({message: "Internal Server Error"})
           
     }
